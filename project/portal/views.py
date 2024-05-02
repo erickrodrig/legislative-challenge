32e8e002a24776f7b392c226e51b273e6dc58e35
Erick Rodrigues <erick.rod1100@gmail.com>
2024-05-02 09:03:04 -0300

    add upload view and update other views

--- project/portal/views.py
+++ project/portal/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from . import models, tables, forms
+from .forms import UploadFile
+from .handlers import file_input_handler
+from django.http import HttpResponse
 
 def home(request):
     return render(request, "home.html")
 
 def bills(request):
-    return render(request, "bills.html")
+    values = tables.BillDetailTable(models.BillDetail.objects.all())
+    return render(request, "bills.html", {"table": values})
 
 def legislators(request):
-    return render(request, "legislators.html")
+    values = tables.LegislatorBillTable(models.LegislatorBill.objects.all())
+    return render(request, "legislators.html", {"table": values})
 
 def upload(request):
-    return render(request, "upload.html")
+    form = UploadFile(request.POST, request.FILES)
+    if form.is_valid():
+        uploaded_content = form.cleaned_data['file']
+        if file_input_handler(uploaded_content.file):
+            return HttpResponse("OK") 
+    return render(request, "upload.html", {"form": UploadFile()})
