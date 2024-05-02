08d6218b7da688f039ffc5c61b535b57b210ed33
Erick Rodrigues <erick.rod1100@gmail.com>
2024-05-01 21:03:39 -0300

    add views to admin panel

--- project/portal/admin.py
+++ project/portal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Legislator)
+admin.site.register(models.Bill)
+admin.site.register(models.Vote)
+admin.site.register(models.VoteResult)
+admin.site.register(models.LegislatorBill)
+admin.site.register(models.BillDetail)
