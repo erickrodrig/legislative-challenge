57d33eff9e270680a99ee04e19dd90a66a9d7396
Erick Rodrigues <erick.rod1100@gmail.com>
2024-05-06 08:16:07 -0300

    Add IDs to admin panel

--- project/portal/admin.py
+++ project/portal/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Legislator)
-admin.site.register(models.Bill)
-admin.site.register(models.Vote)
-admin.site.register(models.VoteResult)
-admin.site.register(models.LegislatorBill)
+
+class LegislatorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    
+class BillAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'sponsor_id']
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'bill_id']
+    
+class VoteResultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'legislator_id', 'vote_id', 'vote_type']
+
+
+admin.site.register(models.Vote, VoteAdmin)
+admin.site.register(models.VoteResult, VoteResultAdmin)
+admin.site.register(models.Legislator, LegislatorAdmin)
+admin.site.register(models.Bill, BillAdmin)
+admin.site.register(models.LegislatorBill,)
 admin.site.register(models.BillDetail)
