e701a054aa3adf52a0ba00886839e7290c07dc68
Erick Rodrigues <erick.rod1100@gmail.com>
2024-05-08 10:04:51 -0300

    add authentication app

--- project/authentication/forms.py
+++ project/authentication/forms.py
@@ -1,1 +1,8 @@
-# TODO: Login / Register forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
--- project/authentication/views.py
+++ project/authentication/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user:
+            auth.login(request, user)
+            return redirect("portal:")
+    form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect("auth:login")
+    return render(request, "register.html", {"form": form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("auth:login")
