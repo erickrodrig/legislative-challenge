f839f5ba669744955da4ada1242e571bc5e9e257
Erick Rodrigues <erick.rod1100@gmail.com>
2024-05-08 10:08:45 -0300

    add url path name

--- project/portal/urls.py
+++ project/portal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('bills/', views.bills, name="bills"),
-    path('legislators/', views.legislators, name="legislators"),
-    path('upload/', views.upload, name="upload"),
+    path("", views.home, name=""),
+    path("bills/", views.bills, name="bills"),
+    path("legislators/", views.legislators, name="legislators"),
+    path("upload/", views.upload, name="upload"),
 ]
